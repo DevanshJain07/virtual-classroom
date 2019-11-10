4ce0bc334e9616f926495a52d10bb9ffb6b7035f
devansh071999 <devanshsampatjain@gmail.com>
2019-11-10 23:46:17 +0530

    help page changed and teach on virtuality added.

--- help1.php
+++ help1.php
@@ -1,154 +1,85 @@
 <html>
     <head>
         <style>
-        div#h1{
-          background-color:rgb(212, 204, 187);
-          height: 50px;
-          width: 100%;
-
-        }
-        div#d1{
-         background-color: blue;
-          background:linear-gradient( #659EC7, #151B8D );
-          width: 100%;
-          height:280px;
-        }
-        .search{
-          width: 30%;
-          position: relative;
-          left:550px;
-          top:120px;
-          display: flex;
-}
-
-.searchTerm {
-  width: 100%;
-  border: 3px solid #00B4CC;
-  border-right: none;
-  padding: 5px;
-  height: 45px;
-  border-radius:30px 0px 0px 30px ;
-  outline: none;
-  color: #9DBFAF;
-}
-
-.searchTerm:focus{
-  color: #00B4CC;
-}
-
-.searchButton {
-  width: 40px;
-  height: 45px;
-  border: 1px solid #00B4CC;
-  background: #00B4CC;
-  text-align: center;
-  color: #fff;
-  border-radius: 0px 30px 30px 0px;
-  cursor:pointer;
-  font-size: 20px;
-}
-/* Style the tab */
-.tab {
-  width:15%;
-  overflow: hidden;
-  border: 1px solid #ccc;
-  background-color: #f1f1f1;
-  position:relative;
-  left:600px;
-}
-
-/* Style the buttons inside the tab */
-.tab button {
-  background-color: inherit;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  padding: 14px 22px;
-  transition: 0.3s;
-  font-size: 17px;
-}
-
-/* Change background color of buttons on hover */
-.tab button:hover {
-  background-color: #ddd;
-}
-
-/* Create an active/current tablink class */
-.tab button.active {
-  background-color: #ccc;
-}
-
-/* Style the tab content */
-.tabcontent {
-  display: none;
-  padding: 6px 12px;
-  border: 1px solid #ccc;
-  border-top: none;
-}
-img{
-  height:20px;
-  width: 20px;
-  position: relative;
-  right: 30px;
-  top:15px;
-}
-.footer{
-  background-color: #d1d0ce;
-  height:150px;
-  width:100%;
-  position: relative;
-  padding-right: 20px;
-  padding-left: 200px;
-  padding-top:40px;
-}
-.link{
-  color: #357EC7;
-  text-decoration: none;
-  size: 8px;
-}
+            .div1links{
+                position: relative;
+                left:250px;
+                font-family: Comic Sans MS;
+                font-size: 15px;
+            }
+            .matter{
+                height:100%;
+                width:60%;
+                position: relative;
+                left:100px;
+                border: none;
+                overflow-wrap: break-word;
+                font-family: "Open Sans", Helvetica, Arial, sans-serif;
+                color:#2c3539;
+                text-align: justify;
+                font-size:17.5px;
+                float:left;
+            }
+            .link{
+                position: relative;
+                top:-10px;
+                height:120%;
+                width:35%;
+                border:1px solid blueviolet;
+                background-color: aliceblue;
+                border-top: none;
+                float:right;
+            }
+            .undln{
+                width:100%;
+                color:blueviolet;
+            }
+            h1{
+                position: relative;
+                left:100px;
+            }
+            h1,h2,h3{
+                color:#0c090a;
+                font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+            }
         </style>
     </head>
     <body>
-      <div id = "h1"></div>
-      <div id = "d1"> <div class="search">
-          <input type="text" class="searchTerm" placeholder="What are you looking for?">
-          <button type="submit" class="searchButton"></button>
-         <img src = "search.png">
-       </div> </div>
-     <div class="tab">
-        <button class="tablinks" onclick="open1(event, 'Student')">Student</button>
-        <button class="tablinks" onclick="open1(event, 'Instructor')">Instructor</button>
-      </div>
-      
-      <div id="Student" class="tabcontent">
-        <h3>Student is good.</h3>
-      </div>
-      
-      <div id="Instructor" class="tabcontent">
-        <h3>Instructor is fantastic.</h3>
-      </div>
-      <script>
-          function open1(evt, user) {
-            var i, tabcontent, tablinks;
-            tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-              tabcontent[i].style.display = "none";
-            }
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-              tablinks[i].className = tablinks[i].className.replace(" active", "");
-            }
-            document.getElementById("user").style.display = "block";
-            evt.currentTarget.className += " active";
-          }
-          </script>
-            <br><br><footer>
-              <hr>
-              <div class = "footer"><pre>
-                <a href = "#" class = "link">Virtuality For Business</a>                      <a href = "#" class = "link">About us</a>                      <a href = "#" class = "link">Topics</a><br>
-                <a href = "#" class = "link">Become an Instructor</a>                         <a href = "#" class = "link">Careers</a>                       <a href = "#" class = "link">Help and Support</a><br>
-                <a href = "#" class = "link">Mobile apps</a>                                  <a href = "#" class = "link">Blogs</a>                         <a href = "#" class = "link">Affiliate</a>
-              </pre></div>
-            </footer> 
-       </body>
-</html>
+        <div style="background-color: aliceblue;">
+            <hr style="border: none;">
+        <div class = "div1links">Virtuality >>FAQs</div>
+        <h1>Virtuality FAQs</h1><hr class = "undln"></div>
+        <div class = "matter">Virtuality's mission is to improve the style of learning.We have multiple courses over multiple areas<br> of education and research
+             and you can take up any course of your choice and study. There is no <br>boundation over what is your qualification or what are you studying.
+            <br><br>Just pick up any course you love and go ahead.
+        <br><br>Learning is endless and so are your opportunities here!!<br><br>
+          <h2>What do Virtuality Courses include ?</h2>
+              Each Virtuality course is created, owned and managed by the instructor(s). 
+              The foundation <br>of each Virtuality course are its lectures, which can include videos, slides, text and additional resources <br>the instructor has uploaded.
+              In addition, instructors can add quizzes, practice tests, assignments and <br>coding exercises, as a way to enhance the learning experience of students.  <br><br>
+        <h2>How do I take a Virtuality Course ?</h2>
+              Virtuality courses are entirely on-demand, which means that you can enroll in the courses that interest<br> you and learn at your own pace.<br>
+              You can begin the course whenever you like, and there are no deadlines to complete it.<br>
+              Virtuality courses can be accessed from several different devices and platforms, including a desktop,<br> laptop, and our Android or iOS mobile app.<br>
+              Detailed information on Virtuality’s platforms and features can be reviewed here.<br>
+              After you enroll in a course, you can access it by clicking on the course link you will receive in your<br> confirmation email,<br>
+              provided you’re logged into your Virtuality account. You can also begin the course by logging in and <br>navigating to your My Courses page.<br>
+              For steps on how to log into your Virtuality account, please click here.<br>
+        <h2>How long do I have to take the Virtuality course?</h2>
+             As noted above, there is no deadlines for completing the course. Even after completing the course, you <br>will have complete
+             access to it. 
+             <h2>How to request a refund?</h2>
+                             You can quickly request a refund by contacting our support team. The request is processed 
+                             and <br>refund is being transferred to your account within 7 days.
+                             <h2>What about Lifetime Access?</h2>
+                             Yes, once you have bought your course, you gain lifetime access for it, provided that your<br> account’s in good standing
+                             and Udemy continues to have a license to the course. So, if you wish to review <br>specific content in the course after 
+                             you finish it, or take it all over again, you can. This scheme also applies<br> for <em>Free courses</em>.
+                             <h2>What if I have issues with my Account/Profile?</h2>
+                             <strong>No worries buddies!!</strong>If you have any queries or troubleshooting related to your Account/Profile,<br>
+                             contact us on our given phone number or email and we will call/mail you back immediately and they will be solved.
+        <h3>If you have more queries, Contact Us!!</h3>     
+        </div>
+        <div class = "links"><img src = "helpimg1.jpg" style="width:600px;height:800px;"></div>
+    </body>
+</html> 
